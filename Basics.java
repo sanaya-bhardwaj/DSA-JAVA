3636ecdf26c3293d63c0490753e016dbc05d1aa9
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2023-11-03 15:06:53 +0530

    Update Basics.java

--- Basics.java
+++ Basics.java
@@ -1,6 +1,16 @@
+package Basics;
+
 public class basics{
-    public static void main(String[] args) {
-        System.out.print("Hello");
-        
-    }
+	public static void main(String[] args) {
+		//System.out.println("Hello");
+		//System.out.print("World"); 
+		//print kept the text on the same line, 
+		//while println moved to a new line after each statement.	
+		
+		//PRINTING NUMBERS
+		System.out.println("4");
+		System.out.println(4);
+		System.out.println(4+3); // line performs arithmetic addition and prints the result
+		System.out.println("4+3"); // line treats its content as a string and prints it as-is.
+	}
 }
