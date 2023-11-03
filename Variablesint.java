5f987555ddb444343e5422d6e8cc6ad87d1fec3b
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2023-11-03 15:12:46 +0530

    Create Variablesint.java

---
+++ Variablesint.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class Variablesint {
+	public static void main(String[] args) {
+		int x; //ek dabba allot, we can store only integer
+		//4 bytes allot for making this dabba  
+		x=5; //dabbe mein value dhal di 5
+		System.out.println(x); //output
+		x=4; //x ki value update 
+		System.out.println(x); //updated output
+		System.out.println(x+1);
+		x=x+5; //x mein 5 add kar diya
+		System.out.println(x);
+	}
+}
