bf6c2f6e77929297e4020282e3b89f23087f7a1d
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2023-11-03 15:15:08 +0530

    Create arithmeticoperation.java

---
+++ arithmeticoperation.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class arithmeticoperation {
+
+	public static void main(String[] args) {
+		int x=5;
+		int y=2;
+		System.out.println(x+y); //addition
+		System.out.println(x-y); //subtraction
+		System.out.println(x*y); //multiplication
+		System.out.println(x/y); //divide (quotient)
+	}
+}
