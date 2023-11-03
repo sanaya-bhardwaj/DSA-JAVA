7fd18137424fa41bcd92c8d7c2f349fe8c2244a3
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2023-11-03 15:15:51 +0530

    Create incrementdecrement.java

---
+++ incrementdecrement.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class incrementdecrement {
+
+	public static void main(String[] args) {
+		int x=5;
+		System.out.println(x);
+		x++; //incrementing x by 1
+		//x++ means x=x+1
+		System.out.println(x);
+		x--; //x-- means x=x-1
+		System.out.println(x);
+		++x; //it also means x=x+1
+		System.out.println(x);
+		--x; //it also means x=x-1
+		System.out.println(x);
+	}
+
+}
