64bf4f49e48c670fe497dcda92e6508eb211eabf
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2023-11-06 00:25:21 +0530

    Create percentageof5subjects.java

---
+++ percentageof5subjects.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class percentageof5subjects {
+
+	public static void main(String[] args) {
+		//code for calculating percentage
+		double x1=100; //maths marks
+		double x2=92; //chem marks
+		double x3=90; //english marks
+		double x4=86; //eco marks
+		double x5=77; //physics marks
+		double p=(x1+x2+x3+x4+x5)/5;  //89.0
+		System.out.println(p);
+	}
+
+}
