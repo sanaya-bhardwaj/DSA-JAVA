605fc5267d3f24f2990821d4a8c9f0364e0e01d9
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2023-11-06 00:52:46 +0530

    Create volumeofsphere.java

---
+++ volumeofsphere.java
@@ -0,0 +1,15 @@
+package Basics;
+//wap to display volume of sphere with given radius
+public class volumeofsphere {
+
+	public static void main(String[] args) {
+		double r=3;
+		double pi=3.1415;
+		double volume= 1.3333*pi*r*r*r;
+		System.out.print("Volume of sphere is ");
+		System.out.println(volume);
+		
+
+	}
+
+}
