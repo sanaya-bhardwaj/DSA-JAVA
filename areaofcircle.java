c2714ddc2e3367c048688f0c215d39afd398cca9
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2023-11-06 00:36:40 +0530

    Update areaofcircle.java

--- areaofcircle.java
+++ areaofcircle.java
@@ -6,7 +6,7 @@ public class areaofcircle {
 		double radius=5;
 		double pi=3.1415;
 		double area=pi*radius*radius;
-		System.out.println(area);
+		System.out.println(area); //output=78.53750000000001
 	}
 
 }
