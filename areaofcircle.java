341b5a97ac9cb717334076e2fd39e3476c81c26e
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2023-11-06 00:35:28 +0530

    Create areaofcircle.java

---
+++ areaofcircle.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class areaofcircle {
+
+	public static void main(String[] args) {
+		double radius=5;
+		double pi=3.1415;
+		double area=pi*radius*radius;
+		System.out.println(area);
+	}
+
+}
