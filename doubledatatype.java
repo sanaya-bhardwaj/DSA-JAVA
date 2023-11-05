9144d7dcd89407d0a8947ea7de7b87502f7ca6fc
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2023-11-06 00:09:46 +0530

    Create doubledatatype.java

---
+++ doubledatatype.java
@@ -0,0 +1,22 @@
+package Basics;
+
+public class doubledatatype {
+
+	public static void main(String[] args) {
+		double x=5; //yaha value 5.0 store hogi
+		double y=2; //yaha value 2.0 store hogi
+		double z1=x/y; //yaha value 2.5 store ho jayegi 
+		System.out.println(z1);
+		
+		double z2=5/2; //yaha 5 and 2 int data type mein consider hoga and answer will be in double data type
+		System.out.println(z2);
+		//double gives answers in point 
+
+	}
+
+}
+//   5/2->2
+//   5.0/2->2.5
+//   5/2.0->2.5
+//   5.0/2.0->2.5
+// dono mein se koi bhi num agar point mein ho then it will give ans in points
