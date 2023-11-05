4d6f632fe6a9ef267600c0e1e9c537995aa8ec0e
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2023-11-06 00:31:14 +0530

    Create sub4each60amrks.java

---
+++ sub4each60amrks.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class sub4each60marks {
+//wap to calculate the percentage of 4 subjects of 60 marks each 
+	public static void main(String[] args) {
+		int subject1= 60;
+		int subject2= 60;
+		int subject3= 60;
+		int subject4= 60;
+		
+		double percentage=(subject1 +subject2+ subject3+ subject4)/4;
+		System.out.println(percentage);
+	}
+
+}
