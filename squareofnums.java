ae1a4a93ac069b6907b3078440a6ebb59a987273
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2023-11-06 01:37:10 +0530

    Create squareofnums.java

---
+++ squareofnums.java
@@ -0,0 +1,15 @@
+package Basics;
+import java.util.Scanner;
+public class squareofnums {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int x=sc.nextInt();
+		int square;
+		square=x*x;
+		System.out.println("Square of Number is "+square);
+		
+	}
+
+}
