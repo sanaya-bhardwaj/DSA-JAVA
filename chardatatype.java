8fb941fc49631efe8f5833094496580a8d029dd6
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2023-11-06 19:18:37 +0530

    Create chardatatype.java

---
+++ chardatatype.java
@@ -0,0 +1,19 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class chardatatype {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		char ch= 'A';
+		System.out.println(ch);
+		int x=(int)ch; //typecasting int---->ch
+		System.out.println(x); //ASCII VALUE 
+    //---ASCII(American Standard Code for Information Interchange)assigns standard numeric values to letters, numerals, punctuation marks, and other characters used in computers.
+		//a b c d e f g h i g h i j k l m n o p q r s t u v w x y z
+		// A B C D E F G H I J K L N O P Q R S T U V W X Y Z 
+		//! @ # $ % ^ & * ( ) _ { } [ ] / , < > ? ~ ` + = - |
+		// 0 1 2 3 4 5 6 7 8 9
+	}
+}
