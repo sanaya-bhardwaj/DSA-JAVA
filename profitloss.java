0d527f715f405d4d7186a9776884ae7befb9c330
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2023-11-11 23:55:26 +0530

    Update profitloss.java

--- profitloss.java
+++ profitloss.java
@@ -1,5 +1,7 @@
 package Conditional;
-//if cost price and selling price of an item is input through the keyboard, write a program to determine whether the seller has made profit or incurred loss.Also determine how much profit he made or loss he incurred.
+//if cost price and selling price of an item is input through the keyboard, 
+//write a program to determine whether the seller has made profit or incurred loss.
+//Also determine how much profit he made or loss he incurred.
 import java.util.Scanner;
 public class profitloss {
 
