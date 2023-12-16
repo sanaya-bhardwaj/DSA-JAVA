7ab6ecf3de6925ef06463160a88cc23bb2928687
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2023-12-16 14:25:59 +0530

    Create threepointsonline.java

---
+++ threepointsonline.java
@@ -0,0 +1,19 @@
+package Conditional;
+import java.util.Scanner;
+//Given three points (x1,y1),(x2,y2) and (x3,y3), write a program to check if all the three points fall on one straight line.
+public class threepointsonline {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double x1 = 1, x2=2, x3=3, y1=1, y2=2, y3=3;
+		double m1=(y2-y1)/(x2-x1);
+		double m2=(y3-y2)/(x3-x2);
+		if(m1==m2) {
+			System.out.println("The three points lie on a single line");
+		}
+		else {
+			System.out.println("they do not lie on a single line");
+		}
+	}
+
+}
