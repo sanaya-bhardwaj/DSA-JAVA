4a597b9be48a70f23c74f97c6a2fe51b2dae66a4
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-02 01:06:38 +0530

    Create Reverse A String.java

---
+++ Reverse A String.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+		String str="ABCD";
+		String rev=" ";
+		
+		int len=str.length(); //4
+		for (int i=len-1; i>=0; i--){ //3 //2//1//0//-1
+		    rev=rev+str.charAt(i); //DCBA
+		}
+		System.out.println("Reverse string is:"+ rev);
+		
+	}
+}
