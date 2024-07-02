8c536fd572500295af9452fe3933e313165ebd20
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-02 15:32:41 +0530

    Update Number of Digits in a Number.java

--- Number of Digits in a Number.java
+++ Number of Digits in a Number.java
@@ -1,1 +1,16 @@
 //Number = 145678 ....number of digits = 6
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int num =123456;
+        int count = 0;
+        while(num>0){
+            num=num/10;
+            count++;
+        }
+        
+        System.out.println("Number of digits:" +count);
+    }
+}
