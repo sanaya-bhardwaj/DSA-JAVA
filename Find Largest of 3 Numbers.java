9f7518ba10b80bc95a32559455e43af425095bd3
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-07-02 15:57:13 +0530

    Update Find Largest of 3 Numbers.java

--- Find Largest of 3 Numbers.java
+++ Find Largest of 3 Numbers.java
@@ -1,4 +1,32 @@
 //a=10 b=20 c=30
-//a>b and a>c --> a largest
-//b>a and c>b -->b largest
+//a>b and a>c --> a is largest
+//b>a and c>b -->b is largest
 //c>a and c>b -->c is largest
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        System.out.println("Enter First Number:");
+        int a=sc.nextInt();
+        
+        System.out.println("Enter Second Number:");
+        int b= sc.nextInt();
+        
+        System.out.println("Enter Third Number:");
+        int c=sc.nextInt();
+        
+        //Approch1- logic 
+        
+        if(a>b && a>c){
+            System.out.println(a+ "is Largest Number");
+        } 
+        else if(b>a && b>c){
+            System.out.println(b+ "is largest Number");
+        }
+        else{
+            System.out.println(c+ "is largest number");
+        }      
+    }
+}
