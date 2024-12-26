0ea313fbbdc0a31fc36cfe88e39ab084feca07f0
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-12-26 19:08:18 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,38 @@
+public static void main(String args[]){
+  //concatenation 
+  String first Name ="tony";
+  String lastName= "stark";
+  String fullName =firstName + "@" + lastName;
+  //tony@stark
+  System.out.println(fullName.length());
+
+  //charAt
+  for(int i=0; i<fullName.length();i++){
+      System.out.println(fullName.charAt(i));
+      }
+  }
+}
+
+
+//compare
+String name1="tony";
+String name2="tony";
+
+//s1>s2 : +ve value
+//s1 == : 0
+//s1<s2 : -ve
+
+if(name1.compare(name2)==0){
+  System.out.println("Strings are equal");
+} else {
+  System.out.println("String are not equal");
+}
+
+//SUBSTRING
+String sentence = " my name is tony";
+//substring(beg index, end index)
+String name = sentence.substring(11,sentence.length());
+System.out.println(name);
+  
+
+    
